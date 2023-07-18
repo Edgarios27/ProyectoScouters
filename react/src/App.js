77417544ad2f7705dfd4b37f58cc0f57b3f067c7
Alex <alejandro.902019@hotmail.com>
2023-07-19 00:49:53 +0200

    prueba

--- react/src/App.js
+++ react/src/App.js
@@ -1,36 +1,13 @@
-import React from 'react';
-import './App.css';
-import { Registro } from './Pages/LoginYRegistro/Registro';
-import { Login } from './Pages/LoginYRegistro/Login';
-import { AuthContext } from './AuthContext/AuthContext';
-import { useContext } from 'react';
-import { BrowserRouter,Routes,Route,Navigate, } from 'react-router-dom';
-import { Error } from './Pages/LoginYRegistro/Error';
-import { Cuenta } from './Pages/LoginYRegistro/Cuenta';
-import { Prueba } from './Pages/LoginYRegistro/Prueba';
-
-
 
 
 
 export function App() {
-  const {isLoggedIn } = useContext(AuthContext);
+
   return ( 
    
+  <></>
+     
   
-      <BrowserRouter> 
-          <Routes>
-            {/* <Route path="/" element={<Navbar/>} /> */}
-            <Route path='/login' element={<Login/>} />
-            <Route path='/error' element={<Error/>} />
-            <Route path='/registro' element={<Registro/>} />
-            <Route path='/cuenta' element={<Cuenta/>} />
-            <Route path='/prueba' element={<Prueba/>} />
-
-             {/* <Route path='/confirm/:token'element={isLoggedIn ? <Login /> : <Navigate to='/login'/>} /> */}
-             <Route path='/cuenta'element={isLoggedIn ? <Cuenta /> : <Navigate to='/login'/>} />
-          </Routes>
-    </BrowserRouter>
 
   );
 }
